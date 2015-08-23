33607f4f081c14e372df87517bb9e37282d777b6
LisaLoop <lisa.lehrman@gmail.com>
2015-08-23 16:02:08 -0700

    made classes for all objects

--- app.js
+++ app.js
@@ -2,13 +2,19 @@
 
 function Game() {
   //Create a new instance of player 1
-  //this.player1 = ...
+  this.player1 = new Player("P1"); //instantiates player
+  console.log(this.player1.name);
+  console.log(this.player1.position);
 
+   //works w/property created in the Player function. 
   //Do the same for a player 2
-  //this.player2 = ...
+  this.player2 = new Player("P2"); //instantiates player
+  console.log(this.player2.name);
+  console.log(this.player2.position);
 
   //Create the track
-  //this.track = ...
+  this.track = new Track();
+  console.log(this.track.$cells);
 }
 
 // `Game.prototype.init` kicks off a new game with a board and two players
@@ -17,25 +23,26 @@ Game.prototype.init = function() {
 };
 
 // A starter Player constructor.
-function Player(team) {
-  //this.name = ...
-  //this.position = ...
+function Player(team) { //player class which defines the properties of a player.
+  this.name = team; 
+  this.position = 0; //starting position
 };
 
 // Remember: prototypes are shared functions between all game instances
 Player.prototype.move = function() {
-  //update player's position
+  this.position++; //increments the position by 1 each time
+  return this.position; //returns the player's position
 };
 
 
 // A starter Track constructor.
 function Track() {
   //Tracks the cells of the board instance
-  //this.$cells = ...
+  this.$cells = $( "#track1 .position" ); //
 
   //Store any other properties that board may have below, such as a reset option
 };
 
 // Start the game!
-var game = new Game();
+var game = new Game();  //follows function Game()
 game.init();
